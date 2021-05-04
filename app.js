ab27de89abcc76210223467762c3844c5ee55007
susmit31 <gohanmysticdbz@gmail.com>
2021-05-05 00:35:56 +0600

    slidin turned off, kept only fadein

--- app.js
+++ app.js
@@ -138,8 +138,8 @@ let slideInSec = ()=>{
 	console.log(secs);
 	
 	const options = {
-		threshold: .1,
-		rootMargin: "0px 0px 100px 0px"
+		threshold: .25,
+		rootMargin: "-200px 0px 100px 0px"
 	}
 	const callback  = (entries, observer)=>{
 		entries.forEach(entry=>{
