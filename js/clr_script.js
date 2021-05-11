4cd606d7c1acfd69272774dd72d0702d71638714
susmit31 <gohanmysticdbz@gmail.com>
2021-05-11 21:54:00 +0600

    completed colours page

--- js/clr_script.js
+++ js/clr_script.js
@@ -1,25 +1,53 @@
-const colours = [
-	'purple',
-	'tomato',
-	'mediumseagreen',
-	'yellow',
-	'aqua',
-	'slateblue',
-	'dodgerblue',
-	'cadetblue',
-	'chocolate',
-	'blueviolet'
-]
+const clr_fam = {
+	grays: [
+		'mediumturquoise','turquoise','cadetblue',
+	],
+	whites: [
+		'wheat', 'seashell', 'lavenderblush',		
+	],
+	yellows: [
+		'yellow','greenyellow', 'gold',
+	],
+	greens:[
+		'lawngreen','lime',
+		'limegreen', 'mediumseagreen', 'yellowgreen',
+		'springgreen', 'palegreen', 'lightgreen'
+	],
+	blues: [
+		'purple', 'dodgerblue', 'blueviolet', 
+		'slateblue', 'aqua',
+		'magenta', 'mediumblue', 
+	],
+	reds: [
+		'tomato', 'peru', 'orangered',
+		'lightcoral', 'lightsalmon', 'orange',
+	]
+};
 
-let container = document.querySelector('.container');
+let target = document.querySelector('.target');
 
-colours.forEach(clr=>{
-	let div = document.createElement('div');
-	div.style.background = clr;
-	container.appendChild(div);
-	div.addEventListener('mouseover', e=>{
-		e.target.textContent = clr;
-		setTimeout(()=>{e.target.textContent='';}, 200);
-		console.log(clr);
+for (family in clr_fam){
+	let container = document.createElement('div');
+	container.classList.add('container');
+	let heading = document.createElement('h1');
+	heading.textContent = family;
+	container.appendChild(heading);
+	
+	clr_fam[family].forEach(clr=>{
+		let block = document.createElement('div');
+		block.style.background = clr;
+		
+		let textContainer = document.createElement('div');
+		let text = document.createElement('p');
+		text.textContent = clr;
+		text.style.display='inline';
+		text.style.overflowWrap = 'break-word';
+		text.classList.add('text');
+		
+		textContainer.appendChild(text)
+		block.appendChild(textContainer);
+		container.appendChild(block);
 	});
-});
+	
+	target.appendChild(container);
+}
