22f4b423ed8e80b8f4409172804ff4711ed6ffc9
susmit31 <gohanmysticdbz@gmail.com>
2021-05-11 22:56:40 +0600

    changed to hex

--- js/clr_script.js
+++ js/clr_script.js
@@ -26,20 +26,54 @@ const clr_fam = {
 
 let target = document.querySelector('.target');
 
+const decimal_to_hex = num=>{
+	if (num<16){
+		if (num==10) return 'A';
+		else if (num>10){
+			return String.fromCharCode('A'.charCodeAt() + num-10);
+		}
+		else return ''+num;
+	}
+	
+	else{
+		let remainder = num % 16;
+		let ratio = Math.floor(num/16);
+		return decimal_to_hex(ratio)+decimal_to_hex(remainder);
+	}
+}
+
+
+const rgb_to_hex = (r,g,b) => {
+	let hexes = [r,g,b].map(c=>decimal_to_hex(c)).map(c=> c.length<2?'0'+c:c);
+	let [r_,g_,b_] = hexes;
+	return `#${r_}${g_}${b_}`;
+}
+
+let count = 0;
 for (family in clr_fam){
 	let container = document.createElement('div');
 	container.classList.add('container');
 	let heading = document.createElement('h1');
 	heading.textContent = family;
 	container.appendChild(heading);
+	target.appendChild(container);
 	
 	clr_fam[family].forEach(clr=>{
 		let block = document.createElement('div');
-		block.style.background = clr;
+		block.style.backgroundColor = clr;
+		block.classList.add(family);
+		container.appendChild(block);
 		
 		let textContainer = document.createElement('div');
 		let text = document.createElement('p');
-		text.textContent = `${clr}\n(click to copy)`;
+		
+		let hexclr = window.getComputedStyle(block).backgroundColor;
+		hexclr = hexclr.slice(3,).split(',');
+		hexclr = hexclr.map(a=>a.replace('(','').replace(')',''));
+		hexclr = hexclr.map(a=>parseInt(a));
+		hexclr = rgb_to_hex(...hexclr);
+		
+		text.textContent = `${hexclr}\n(click to copy)`;
 		text.style.display='inline';
 		text.style.overflowWrap = 'break-word';
 		text.classList.add('text');
@@ -47,12 +81,11 @@ for (family in clr_fam){
 		textContainer.appendChild(text)
 		block.appendChild(textContainer);
 		block.addEventListener('click', e=>{
-			navigator.clipboard.writeText(clr);
+			console.log(hexclr);
+			navigator.clipboard.writeText(hexclr);
 			alert('copied!');
 		});
 		
-		container.appendChild(block);
 	});
 	
-	target.appendChild(container);
 }
