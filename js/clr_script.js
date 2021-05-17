8d0e693199a6690ba3ec0b2e69dccebab2e03c19
susmit31 <gohanmysticdbz@gmail.com>
2021-05-17 23:46:10 +0600

    changed delta_saturation and delta_lightness

--- js/clr_script.js
+++ js/clr_script.js
@@ -173,13 +173,13 @@ const hsl_to_rgb = (h,s,l)=>{
 const make_palette = (r,g,b)=>{
 	let hsl = rgb_to_hsl(r,g,b);
 	let palette = [];
-	let delta_l = hsl[2]/3;
-	let delta_s = hsl[1]/5;
+	let delta_l = hsl[2]/5;
+	let delta_s = hsl[1]/3;
 	
 	for (let i=0; i<3; i++){
 		let clr = [...hsl];
-		clr[2] -= delta_l*i*Math.pow(-1,i);
-		clr[1] -= delta_s*i*Math.pow(-1,i);
+		clr[2] += delta_l*i*Math.pow(-1,i);
+		clr[1] += delta_s*i*Math.pow(-1,i);
 		palette.push(clr);
 	}
 	
