244a1141b0cceb966754c31382ef6b49e28536dc
susmit31 <gohanmysticdbz@gmail.com>
2021-05-22 21:47:39 +0600

    added toggle fnality; changed btn clrs

--- bokeh-ml/app.js
+++ bokeh-ml/app.js
@@ -13,33 +13,27 @@ const constraints = {
 }
 
 const getCamera = async()=>{
-	const devices = await navigator.mediaDevices.enumerateDevices();
-	const videoDevices = devices.filter(device=>device.kind=='videoinput');
-	
 	if (streamStarted){
 		video.play();
 		return
 	}
 	
 	if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia){
-		startStream();
+		const stream = await navigator.mediaDevices.getUserMedia(constraints);
+		video.srcObject = stream;
+		streamStarted = true;
 	}
 }
 
-const startStream = async()=>{
-	const stream = await navigator.mediaDevices.getUserMedia(constraints);
-	video.srcObject = stream;
-	streamStarted = true;
-}
-
 let startButton = document.querySelector('button.start');
-startButton.onclick = ()=>getCamera();
+startButton.onclick = ()=>{
+	getCamera();
+	video.classList.remove('d-none');
+}
 
 let stopButton = document.querySelector('button.stop')
 
 stopButton.onclick = ()=>{
 	video.pause();
-	video.classList.add('d-none');
-	button.innerText = 'Start';
-	
+	video.classList.add('d-none');	
 }
