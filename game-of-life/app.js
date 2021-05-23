569ef9e13e2a8d91670f7e63ee216c0f68c4456c
susmit31 <gohanmysticdbz@gmail.com>
2021-05-23 21:28:09 +0600

    enabled smoother path drawing on pc; testing on phone

--- game-of-life/app.js
+++ game-of-life/app.js
@@ -1,10 +1,15 @@
-const N_ROW = 28;
-const N_COL = 18;
-
 const ACTIVE_CLR = 'rgb(255, 255, 255)';
 const INACTIVE_CLR = '#5F9EA0';
+let simuln = null;
 
 let game = document.querySelector('#game');
+console.log();
+
+const N_COL = 18;
+const BOX_SIZE = parseInt(
+		window.getComputedStyle(game).width
+	)/ N_COL;
+const N_ROW = Math.floor(window.innerHeight * .9/BOX_SIZE);
 
 Array(N_ROW*N_COL).fill(0).forEach(i=>{
 	game.appendChild(document.createElement('div'));
@@ -25,20 +30,57 @@ let sqrs_grid = (()=>{
 
 Array.from(sqrs).forEach(c=>{
 	c.style.height = window.getComputedStyle(c).width;
-	c.addEventListener('click',e=>{
-		if (e.target.style.backgroundColor!==ACTIVE_CLR)
-			e.target.style.backgroundColor = ACTIVE_CLR;
-		else e.target.style.backgroundColor = INACTIVE_CLR;
-	})
 });
 
+const drawOnDrag = e=>{
+	if (simuln!==null){
+		clearInterval(simuln);
+		console.log('hi');
+		simuln = null;
+	}
+	if (e.target.style.backgroundColor!==ACTIVE_CLR)
+		e.target.style.backgroundColor = ACTIVE_CLR;
+	else e.target.style.backgroundColor = INACTIVE_CLR;
+};
+
+game.onmousedown = ()=>{
+	Array.from(sqrs).forEach(c=>{
+		c.addEventListener('mouseenter',drawOnDrag);
+	});
+};
+
+const mobileDrawOnDrag = e=>{
+	let x = e.clientX, y = e.clientY;
+	console.log(x);
+	console.log(y);
+	toggleLife(document.elementFromPoint(x,y))
+}
+
+game.ontouchstart = ()=>{
+	Array.from(sqrs).forEach(c=>{
+		c.addEventListener('touchmove',mobileDrawOnDrag);
+	});
+};
+
+game.onmouseup = ()=>{
+	Array.from(sqrs).forEach(c=>{
+		c.removeEventListener('mouseenter',drawOnDrag);
+	})
+};
+
+game.ontouchend = ()=>{
+	Array.from(sqrs).forEach(c=>{
+		c.removeEventListener('touchmove',mobileDrawOnDrag);
+	})
+};
+
+game.ontouchend
 const range = n => n==1? [0] : [...range(n-1),n-1];
 
 const findNeighbours = sqr => {
 	let neighbours = [];
 	let idx = Array.from(sqrs).findIndex(s=>s==sqr);
 	let pos_x = Math.floor(idx/N_COL), pos_y = idx%N_COL;
-	console.log([pos_x,pos_y]);
 	
 	const start_x = pos_x > 0? -1 : 0;
 	const end_x = pos_x < N_ROW-1? 1 : 0;
@@ -72,24 +114,28 @@ let simBtn = document.querySelector('.sim');
 let resetBtn = document.querySelector('.reset');
 
 resetBtn.addEventListener('click', e=>{
-	Array.from(sqrs).forEach(s=>{
-		s.style.backgroundColor = INACTIVE_CLR;
-	});
+	window.location.reload();
 });
 
 simBtn.addEventListener('click', e=>{
-	setInterval(()=>{
-		Array.from(sqrs).forEach(sqr=>{
-			let neighs = findNeighbours(sqr);
-			const live_neighs = neighs.map(n=>isAlive(n)).reduce((a,b)=>a+b);
-			if (isAlive(sqr)){
-				if (live_neighs==2 || live_neighs==3) null;
-				else toggleLife(sqr);
-			}
-			else{
-				if (live_neighs==3) toggleLife(sqr);
-				else null;
-			}
-		});
-	},500);
+	if (simuln===null){
+		simuln = setInterval(()=>{
+			Array.from(sqrs).forEach(sqr=>{
+				let neighs = findNeighbours(sqr);
+				const live_neighs = neighs.map(n=>isAlive(n)).reduce((a,b)=>a+b);
+				if (isAlive(sqr)){
+					if (live_neighs==2 || live_neighs==3) null;
+					else toggleLife(sqr);
+				}
+				else{
+					if (live_neighs==3) toggleLife(sqr);
+					else null;
+				}
+			});
+		},500);
+		simBtn.addEventListener('click',e=>{
+			clearInterval(simuln);
+			simuln = null;
+		})
+	}
 });
