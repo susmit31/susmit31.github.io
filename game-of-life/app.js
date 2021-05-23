60d46d5b363e33b790e7fdd500a0b6462368a960
susmit31 <gohanmysticdbz@gmail.com>
2021-05-23 19:28:18 +0600

    added game of life code - functionality complete, styling remains

---
+++ game-of-life/app.js
@@ -0,0 +1,95 @@
+const N_ROW = 28;
+const N_COL = 18;
+
+const ACTIVE_CLR = 'rgb(255, 255, 255)';
+const INACTIVE_CLR = '#5F9EA0';
+
+let game = document.querySelector('#game');
+
+Array(N_ROW*N_COL).fill(0).forEach(i=>{
+	game.appendChild(document.createElement('div'));
+});
+
+let sqrs = game.children;
+let sqrs_grid = (()=>{
+	let grid = [];
+	for (let i = 0; i<N_ROW; i++){
+		let row = [];
+		for (let j =0; j<N_COL; j++){
+			row.push(sqrs[N_COL*i + j]);
+		}
+		grid.push(row);
+	}
+	return grid;
+})();
+
+Array.from(sqrs).forEach(c=>{
+	c.style.height = window.getComputedStyle(c).width;
+	c.addEventListener('click',e=>{
+		if (e.target.style.backgroundColor!==ACTIVE_CLR)
+			e.target.style.backgroundColor = ACTIVE_CLR;
+		else e.target.style.backgroundColor = INACTIVE_CLR;
+	})
+});
+
+const range = n => n==1? [0] : [...range(n-1),n-1];
+
+const findNeighbours = sqr => {
+	let neighbours = [];
+	let idx = Array.from(sqrs).findIndex(s=>s==sqr);
+	let pos_x = Math.floor(idx/N_COL), pos_y = idx%N_COL;
+	console.log([pos_x,pos_y]);
+	
+	const start_x = pos_x > 0? -1 : 0;
+	const end_x = pos_x < N_ROW-1? 1 : 0;
+	const start_y = pos_y > 0? -1 : 0;
+	const end_y = pos_y < N_COL-1? 1 : 0;
+	for (let i = start_x; i<=end_x; i++){
+		for (let j=start_y; j<=end_y; j++){
+			if (i!==0 || j!==0){
+				let neigh = sqrs[idx + N_COL*i + j]
+				if (neigh)
+					neighbours.push(neigh);
+			}
+		}
+	}
+	
+	return neighbours
+}
+
+const isAlive = sqr=>
+	window.getComputedStyle(sqr).backgroundColor == ACTIVE_CLR? 
+	true : false;
+
+const toggleLife = sqr=>{
+	if (isAlive(sqr)) sqr.style.backgroundColor = INACTIVE_CLR;
+	else sqr.style.backgroundColor = ACTIVE_CLR;
+}
+
+
+
+let simBtn = document.querySelector('.sim');
+let resetBtn = document.querySelector('.reset');
+
+resetBtn.addEventListener('click', e=>{
+	Array.from(sqrs).forEach(s=>{
+		s.style.backgroundColor = INACTIVE_CLR;
+	});
+});
+
+simBtn.addEventListener('click', e=>{
+	setInterval(()=>{
+		Array.from(sqrs).forEach(sqr=>{
+			let neighs = findNeighbours(sqr);
+			const live_neighs = neighs.map(n=>isAlive(n)).reduce((a,b)=>a+b);
+			if (isAlive(sqr)){
+				if (live_neighs==2 || live_neighs==3) null;
+				else toggleLife(sqr);
+			}
+			else{
+				if (live_neighs==3) toggleLife(sqr);
+				else null;
+			}
+		});
+	},500);
+});
