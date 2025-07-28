66de81151adc25f739c7b3233848e69bd5bd24d9
Sarif Mohammad Tasnim Islam <gohanmysticdbz@gmail.com>
2025-07-28 18:11:50 +0600

    netlified

---
+++ timer-netlify/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "timer-cache-v2";
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./android-chrome-512x512.png",
+        "./android-chrome-192x192.png",
+        "./manifest.json",
+        "./sw.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
